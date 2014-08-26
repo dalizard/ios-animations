fbef467b5a170ba495573ba43d2df7ee8616e035
Dimitar Haralanov <dharalanov@me.com>
2014-08-26 21:36:21 +0300

    Add Podfile with JNWSpringAnimation

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,14 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// JNWSpringAnimation
+#define COCOAPODS_POD_AVAILABLE_JNWSpringAnimation
+#define COCOAPODS_VERSION_MAJOR_JNWSpringAnimation 0
+#define COCOAPODS_VERSION_MINOR_JNWSpringAnimation 5
+#define COCOAPODS_VERSION_PATCH_JNWSpringAnimation 0
+
