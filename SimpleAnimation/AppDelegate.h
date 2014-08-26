c175fb720ca6555940617174903174ad9cfe0a99
Dimitar Haralanov <dharalanov@me.com>
2014-08-26 20:48:31 +0300

    Initial commit

---
+++ SimpleAnimation/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SimpleAnimation
+//
+//  Created by Dimitar Haralanov on 8/26/14.
+//  Copyright (c) 2014 Dimitar Haralanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SimpleAnimation/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  SimpleAnimation
+//
+//  Created by Dimitar Haralanov on 8/26/14.
+//  Copyright (c) 2014 Dimitar Haralanov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
